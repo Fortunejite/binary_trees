7afdb3599c1aedb76a4ca31ff166e0e476791171
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2023-02-28 12:26:50 -0800

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,6 +3,7 @@
 /**
  * binary_tree_height - calculates the height of the tree
  * @tree: target
+ * Return: 1 on success
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -10,6 +11,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_l;
 	size_t height_r;
 
+	if (table == NULL)
+		return (0);
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (height_l > height_r ? height_l : height_r);
