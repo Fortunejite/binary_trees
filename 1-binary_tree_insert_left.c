bc827192d0215b845e6e8c833ffaea9a59655d95
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2023-02-28 12:41:29 -0800

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,10 +15,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
-	new->n = value;
-	new->left = NULL;
-	new->right = NULL;
-	new->parent = parent;
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
 	if (parent->left == NULL)
 	{
 		parent->left = new;
