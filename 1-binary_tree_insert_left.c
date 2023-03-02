c3236465c6bf558558637a946f4078a70eea29f5
Fortune <fortunejite1234@gmail.com>
2023-03-02 03:55:47 -0800

    ‘m’

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,28 +1,33 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - insert node at the left of the tree
+ * binary_tree_insert_right - insert node at the left of the tree
  * @parent: the parent of the tree
  * @value: value of the new node
  *
  * Return: the new node
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
+	if (parent == NULL)
+		return (NULL);
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
-	new = binary_tree_node(parent, value);
-	if (new == NULL)
-		return (NULL);
-	if (parent->left != NULL)
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
+	if (parent->left == NULL)
 	{
-		new->left = parent->left;
-		parent->left->parent = new;
+		parent->left = new;
+		return (new);
 	}
+	new->left = parent->left;
+	parent->left->parent = new;
 	parent->left = new;
 	return (new);
 }
